b1e5ad93c465f70fbb63df386d6adea9e518aa34
siva007-sk <sivapatro12@gmail.com>
2022-08-19 19:28:59 +0530

    completed fitness trainer form

--- angular-spa/src/app/place-fitness-trainer-appointment/place-fitness-trainer-appointment.component.ts
+++ angular-spa/src/app/place-fitness-trainer-appointment/place-fitness-trainer-appointment.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup } from "@angular/forms";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { from } from "rxjs";
+import { UserService } from "../_services";
 
 export class Fitness {
   constructor(
@@ -28,9 +30,63 @@ export class Fitness {
 export class PlaceFitnessTrainerAppointmentComponent implements OnInit {
   fitnessForm: FormGroup;
 
-  constructor() {}
+  constructor(private fb: FormBuilder, private _service: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fitnessForm = this.fb.group({
+      name: ["", Validators.required],
+      age: ["", Validators.required],
+      email: ["", Validators.required],
+      phoneNumber: ["", Validators.required],
+      addressLine1: ["", Validators.required],
+      addressLine2: ["", Validators.required],
+      city: ["", Validators.required],
+      state: ["", Validators.required],
+      country: ["", Validators.required],
+      pinCode: ["", Validators.required],
+      trainerPreference: ["", Validators.required],
+      needPhysioTherapist: ["", Validators.required],
+      package: ["", Validators.required],
+      week: ["", Validators.required],
+      amount: ["", Validators.required],
+    });
+  }
 
-  onSubmit() {}
+  onSubmit() {
+    if (this.fitnessForm.valid) {
+      const payload = this.generatePayload(this.fitnessForm.getRawValue());
+      this._service.postfitnessdata(payload);
+    }
+  }
+
+  generatePayload(form) {
+    const payload: Fitness = {
+      firstname: form.name.split(" ").slice(1) || "",
+      lastname: form.name.split(" ")[0] || "",
+      age: form.age,
+      email: form.email,
+      phonenumber: form.phoneNumber,
+      streetaddress: `${form.addressLine1}, ${form.addressLine2}`,
+      city: form.city,
+      state: form.state,
+      country: form.country,
+      pincode: form.pinCode,
+      trainerpreference: form.trainerPreference,
+      physiotherapist: form.needPhysioTherapist,
+      packages: form.package,
+      inr: form.amount,
+      paisa: 0,
+    };
+    return payload;
+  }
+
+  calculateAmount() {
+    const packageValue = this.fitnessForm.get("package").value
+      ? parseInt(this.fitnessForm.get("package").value)
+      : 0;
+    const weeks = this.fitnessForm.get("week").value
+      ? this.fitnessForm.get("week").value
+      : 0;
+    this.fitnessForm.get("amount").setValue(packageValue * weeks);
+  }
 }
